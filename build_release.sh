61c3f41315455f7ef8c78f302162dbe210535994
Beinsezii <beinsezii@gmail.com>
2022-06-08 04:44:41 -0700

    Improved build script

---
+++ build_release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+BIN="gui"
+
+LINUX="./target/release/${BIN}"
+WIN="./target/release/${BIN}.exe"
+
+cargo build --release --features=gui
+
+mkdir ./bin 2>/dev/null
+
+if [ -f "$LINUX" ]; then
+    cp "$LINUX" ./bin/pixelbuster
+    cargo build --release --features=gui --target x86_64-pc-windows-gnu
+    cp "./target/x86_64-pc-windows-gnu/release/${BIN}.exe" ./bin/pixelbuster.exe
+fi
+
+if [ -f "$WIN" ]; then
+    cp "$WIN" ./bin/pixelbuster.exe
+    cargo build --release --features=gui --target x86_64-unknown-linux-gnu
+    cp "./target/x86_64-unknown-linux-gnu/release/${BIN}" ./bin/pixelbuster
+fi
