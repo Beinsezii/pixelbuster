31d3d7771062cbf9846e5f11f2fcc218b6f0decd
Beinsezii <beinsezii@gmail.com>
2022-07-01 20:12:15 -0700

    GUI ok so THIS one should be SRGB still...

--- src/bin/gui/gui.rs
+++ src/bin/gui/gui.rs
@@ -99,7 +99,7 @@ impl App for PBGui {
                                     let mut newimg = img.to_rgba32f();
                                     pixelbuster(
                                         &self.code,
-                                        Space::LRGB,
+                                        Space::SRGB,
                                         &mut newimg,
                                         img.width() as usize,
                                         None,
@@ -313,7 +313,7 @@ impl PBGui {
                 // parse into ops
                 let i_parse = Instant::now();
 
-                let ops = parse_ops(&self.code, Space::LRGB);
+                let ops = parse_ops(&self.code, Space::SRGB);
 
                 self.t_parse = Instant::now() - i_parse;
 
